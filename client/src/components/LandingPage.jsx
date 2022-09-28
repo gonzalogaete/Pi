6f308b54299089292c05bc3d7771b9e40a4956e9
gonzalogaete <gonzalogaete110@gmail.com>
2022-09-28 04:39:48 -0300

    commit

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -7,15 +7,19 @@ import LP from '../css/LP.css';
 export default function LandingPage(){
     return (
         <div class='main'>
+            <div className="cajaTitulo">
+                <img class='title'src={pokemon} width='700px' height='250px'></img>
             <div>
-            <img class='title'src={pokemon} width='700px' height='250px'></img>
-            <div>
-            <img class='pikachu'src={pikachu} width='300px' height='300px'></img>
+            {/* <img class='pikachu'src={pikachu} width='300px' height='300px'></img> */}
             </div>
             </div>
-            <Link to='/home'>
-            <button class="button-32" role="button">Gotcha!</button>
-            </Link>
+            <div className="botonPrincipal">
+                <Link to='/home'>
+                <button class="button-32" role="button">Gotcha!</button>
+                </Link>
+            </div>
+
+            
         </div>
     )
 }
