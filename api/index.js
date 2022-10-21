10f40333eeff4a4076db8a29302029c97054e48b
gonzalogaete <gonzalogaete110@gmail.com>
2022-10-21 14:31:45 -0300

    a

--- api/index.js
+++ api/index.js
@@ -20,9 +20,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
+require("dotenv").config();
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s Esperando Respuesta en 3001'); // eslint-disable-line no-console
   });
 });
